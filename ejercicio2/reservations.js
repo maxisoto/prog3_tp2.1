b252209486c9a53cc9957a281eb5993eafb36c22
maxisoto <maxiservice73@gmail.com>
2024-07-07 22:33:51 -0300

    resolucion punto uno y dos ejercicio 2

--- ejercicio2/reservations.js
+++ ejercicio2/reservations.js
@@ -1,6 +1,38 @@
-class Customer {}
+class Customer {
+    constructor(id, name, email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
-class Reservation {}
+    get info() {
+        return `${this.name} (${this.email})`;
+    }
+}
+
+class Reservation {
+    constructor(id, customer, date, guests) {
+        this.id = id;
+        this.customer = customer;
+        this.date = date;
+        this.guests = guests;
+    }
+
+    get info() {
+        const formattedDate = this.date.toLocaleString(); // Formatear la fecha y hora
+        return `Reserva #${this.id} - ${formattedDate} | Cliente: ${this.customer.info} | Comensales: ${this.guests}`;
+    }
+
+    static validateReservation(reservationData) {
+        const currentDate = new Date();
+        const reservationDate = new Date(reservationData.date);
+
+        if (reservationDate <= currentDate || reservationData.guests <= 0) {
+            return false; // La reserva no es válida
+        }
+        return true; // La reserva es válida
+    }
+}
 
 class Restaurant {
     constructor(name) {
